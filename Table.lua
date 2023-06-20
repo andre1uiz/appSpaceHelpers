5c5190125b3dfa18d5fd25c54049c0cad841489c
ANDRE LUIZ GONCALVES <andre1uiz@hotmail.com>
2023-06-20 19:37:47 -0300

    Add files via upload
    In this version there is no restriction of max number of collumns to be used - function UITable(collumnNames: string+, collumnsValues: auto+) updated

--- Table.lua
+++ Table.lua
@@ -4,57 +4,30 @@
 --*************
 function UITable(
   colNames,
-  refTable,
-  _refTable2,
-  _refTable3,
-  _refTable4,
-  _refTable5)
-  local colNames = colNames or {} -- can be a string or a table with strings
-  local refTable = refTable --values for column1
-  local refTable2 = _refTable2 or nil --values for column2
-  local refTable3 = _refTable3 or nil --values for column3
-  local refTable4 = _refTable4 or nil --values for column4
-  local refTable5 = _refTable5 or nil --values for column5
-  local totalTables = {}
-  local pointerTable = '[{'
+ collumns)
+  local colNames = colNames -- for the collumn names that should be the ID of each collumn
+  local totalCollumns = collumns -- a table that has tables with the values of each collunm
+  local pointerTable = '[{' --string that will be returned to the function
+  local totalValues = #totalCollumns[1]
   --*************
-  local ColType = type(colNames)
-  if ColType == 'string' then
-    for i, element in ipairs(refTable) do
-      pointerTable = pointerTable .. '"' .. colNames .. '":"' .. element .. '"'
-      if i == #refTable then
-        pointerTable = pointerTable .. '}]'
-      else
-        pointerTable = pointerTable .. '}, {'
+if #colNames == #totalCollumns then
+  for i = 1, totalValues, 1 do
+    for index, name in ipairs(colNames) do
+      pointerTable = pointerTable .."\"".. name .."\":\"".. totalCollumns[index][i]
+      if index == #colNames then
+        pointerTable = pointerTable .. "\"}"
+      else 
+        pointerTable = pointerTable .. "\","
       end
     end
-  else
-    if refTable3 == nil then
-      totalTables = {refTable, refTable2}
-    elseif refTable4 == nil then
-      totalTables = {refTable, refTable2, refTable3}
-    elseif refTable5 == nil then
-      totalTables = {refTable, refTable2, refTable3, refTable4}
+    if i == totalValues then
+      pointerTable = pointerTable .. "]"
     else
-      totalTables = {refTable, refTable2, refTable3, refTable4, refTable5}
-    end
-
-    for i = 1, #refTable do
-      for names, colname in ipairs(colNames) do
-        pointerTable =
-          pointerTable ..
-          '"' .. colname .. '":"' .. totalTables[names][i] .. '"'
-        if #colNames > names then
-          pointerTable = pointerTable .. ','
-        end
-      end
-      if i == #refTable then
-        pointerTable = pointerTable .. '}]'
-      else
-        pointerTable = pointerTable .. '}, {'
-      end
+      pointerTable = pointerTable .. ",{"
     end
   end
+end
+  
   return pointerTable
 end
 --*************
